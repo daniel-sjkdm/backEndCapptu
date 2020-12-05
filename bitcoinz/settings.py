c018b67cb6d3c780e24c6c57c2ca388849eb0bd4
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-04 18:24:15 -0600

    Allowed hosts and cors origins

--- bitcoinz/settings.py
+++ bitcoinz/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'localhost'
+    'localhost',
 ]
 
 
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'coinz',
-    'corsheaders'
+    'corsheaders',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -129,7 +130,9 @@ STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
 
+SHELL_PLUS = "ipython"
+
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
 ]
