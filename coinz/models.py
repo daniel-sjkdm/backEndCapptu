9b1ebe6559f7aca17cfc050e22408f161f2000e6
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-04 18:24:45 -0600

    Coin model

--- coinz/models.py
+++ coinz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Coin(models.Model):
+    book = models.CharField(max_length=10)
+    created_at = models.DateTimeField()
+    volume = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    last = models.FloatField(null=True, blank=True)
+    vwap = models.FloatField(null=True, blank=True)
+    ask = models.FloatField(null=True, blank=True)
+    bid = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.pk}: {self.book} - {self.created_at}"
