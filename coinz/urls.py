013e92c203ab00adcdfabcb71ab46c262ce26bd5
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-04 18:23:28 -0600

    API urls

--- coinz/urls.py
+++ coinz/urls.py
@@ -6,7 +6,9 @@ appname = 'coinz'
 urlpatterns = [
     path('api/', api.CoinzAPI.as_view(), name='api'),
     path('api/by-week/', api.CoinzByWeekAPI.as_view(), name='api-by-week'),
+    path('api/by-month/', api.CoinzByMonthAPI.as_view(), name='api-by-month'),
     path('api/by-year/', api.CoinzByYearAPI.as_view(), name='api-by-year'),
     path('api/last-24/', api.CoinzLast24API.as_view(), name='api-last-24'),
-    path('api/date-range/', api.CoinzDateRangeAPI.as_view(), name='api-date-range')
+    path('api/date-range/', api.CoinzDateRangeAPI.as_view(), name='api-date-range'),
+    path('api/repeat/', api.make_bitso_call, name='api-bitso-call')
 ]
